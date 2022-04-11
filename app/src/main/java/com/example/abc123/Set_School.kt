2c6e913aed4da06b98f4dd6b08bc23171330dfbb
JeongSeungHyun <net1645@naver.com>
2022-04-11 17:18:51 +0900

    Test

--- app/src/main/java/com/example/abc123/Set_School.kt
+++ app/src/main/java/com/example/abc123/Set_School.kt
@@ -1,13 +1,12 @@
 package com.example.abc123
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class EditName : AppCompatActivity() {
+class Set_School : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_name)
-
-
+        setContentView(R.layout.activity_set_school)
     }
 }
