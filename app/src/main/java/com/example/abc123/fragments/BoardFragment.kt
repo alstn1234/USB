084e24ef544a1242f9cf8b50d8ad07dde082549e
JeongSeungHyun <net1645@naver.com>
2022-04-11 17:17:16 +0900

    Test

--- app/src/main/java/com/example/abc123/fragments/BoardFragment.kt
+++ app/src/main/java/com/example/abc123/fragments/BoardFragment.kt
@@ -1,13 +1,10 @@
 package com.example.abc123.fragments
 
-import android.R
-import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.abc123.databinding.FragmentBoardBinding
 
@@ -25,6 +22,8 @@ class BoardFragment : Fragment() {
         mBinding = binding
         val item = arrayOf("내가 쓴 글", "내가 댓글 단 글", "자유게시판", "질문게시판", "거래게시판", "단대별 게시판")
         bbinding.listview.adapter = ArrayAdapter(requireActivity()!!, android.R.layout.simple_list_item_1, item)
+
+
             return bbinding?.root
         }
 
--- app/src/main/java/com/example/abc123/fragments/MypageFragment.kt
+++ app/src/main/java/com/example/abc123/fragments/MypageFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.fragment.app.Fragment
-import com.example.abc123.R
 import com.example.abc123.databinding.FragmentBoardBinding
 import com.example.abc123.databinding.FragmentMypageBinding
 
