94d6e11fac331baacffd56262aecb8cd9479c1e2
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2022-05-16 02:35:41 +0900

    Add files via upload

---
+++ main/java/com/example/abc123/Photo.kt
@@ -0,0 +1,9 @@
+package com.example.abc123
+
+class Photo (
+    var imageUrl:String? ="",
+    var nickname:String? ="",
+    var school:String?= "",
+    var major:String?= ""
+){
+}
---
+++ main/java/com/example/abc123/Set_School.kt
@@ -0,0 +1,14 @@
+package com.example.abc123
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class Set_School : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_school)
+        }
+}
